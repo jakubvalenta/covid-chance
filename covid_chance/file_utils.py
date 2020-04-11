554049a4ae0fb1a194baebd09ec688e40da1b858
Jakub Valenta <jakub@jakubvalenta.cz>
2020-04-11 12:34:24 +0200

    file_utils: safe_filename(): Apply max length

--- covid_chance/file_utils.py
+++ covid_chance/file_utils.py
@@ -6,9 +6,9 @@ from pathlib import Path
 from typing import IO, Any, Callable, Iterator, List
 
 
-def safe_filename(s: str) -> str:
+def safe_filename(s: str, max_length: int = 64) -> str:
     short_hash = sha256(s.encode()).hexdigest()[:7]
-    safe_str = re.sub(r'[^A-Za-z0-9_\-\.]', '_', s).strip('_')
+    safe_str = re.sub(r'[^A-Za-z0-9_\-\.]', '_', s).strip('_')[:max_length]
     return f'{safe_str}--{short_hash}'
 
 
